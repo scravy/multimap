bdb17bf392409373fa1f1d9d93a3e3b8dac4a0e0
Nikita Volkov <nikita.y.volkov@mail.ru>
2013-06-25 19:12:39 +0400

    Rename modules and reimplement SetMultiMap (#1, #2)

--- src/Data/BagMultiMap.hs
+++ src/Data/BagMultiMap.hs
@@ -1,5 +1,5 @@
 -- |
--- Module       : Data.MultiMap
+-- Module       : Data.BagMultiMap
 -- Copyright    : (c) Julian Fleischer 2013
 -- License      : MIT (See LICENSE file in cabal package)
 --
@@ -7,10 +7,10 @@
 -- Portability  : non-portable (DeriveDataTypeable)
 -- 
 -- A very simple MultiMap, based on 'Data.Map.Map' from the containers package.
-module Data.MultiMap (
+module Data.BagMultiMap (
 
     -- * MultiMap type
-    MultiMap,
+    BagMultiMap,
 
     -- * Query
     null,
@@ -87,199 +87,199 @@ import Data.Data
 --
 -- Internally this is simply a @Map k [v]@.
 -- See 'toMap' for accessing the underlying 'Map'.
-newtype MultiMap k v = MultiMap (Word32, Word32, Map k [v])
+newtype BagMultiMap k v = BagMultiMap (Word32, Word32, Map k [v])
     deriving (Data, Typeable)
 
 
-null :: MultiMap k a -> Bool
+null :: BagMultiMap k a -> Bool
 -- ^ /O(1)./ Check whether the multimap is empty or not.
-null (MultiMap (_, _, m)) = Map.null m
+null (BagMultiMap (_, _, m)) = Map.null m
 
 
-size :: MultiMap k a -> Int
+size :: BagMultiMap k a -> Int
 -- ^ /O(1)./ The number of elements in the multimap.
-size (MultiMap (_, size, _)) = fromIntegral size
+size (BagMultiMap (_, size, _)) = fromIntegral size
 
 
-numKeys :: MultiMap k a -> Word32
+numKeys :: BagMultiMap k a -> Word32
 -- ^ /O(1)./ The number of keys in the multimap.
 -- 
 -- As this is a multimap, the number of keys is not
 -- necessarily equal to the number of values.
-numKeys (MultiMap (nk, _, _)) = nk
+numKeys (BagMultiMap (nk, _, _)) = nk
 
 
-numValues :: MultiMap k a -> Word32
+numValues :: BagMultiMap k a -> Word32
 -- ^ /O(1)./ The number of values in the multimap.
 --
 -- As this is a multimap, the number of keys is not
 -- necessarily equal to the number of values.
-numValues (MultiMap (_, nv, _)) = nv
+numValues (BagMultiMap (_, nv, _)) = nv
 
 
-notMember, member :: Ord k => MultiMap k a -> k -> Bool
+notMember, member :: Ord k => BagMultiMap k a -> k -> Bool
 -- | /O(log n)./ Is the key a member of the multimap?
-member (MultiMap (_, _, map)) key = Map.member key map
+member (BagMultiMap (_, _, map)) key = Map.member key map
 -- | /O(log n)./ Is the key not a member of the multimap?
 notMember key = not . member key
 
 
-(!) :: Ord k => MultiMap k a -> k -> [a]
+(!) :: Ord k => BagMultiMap k a -> k -> [a]
 -- ^ As @flip lookup@
 (!) = flip lookup
 
 
-lookup :: Ord k => k -> MultiMap k a -> [a]
+lookup :: Ord k => k -> BagMultiMap k a -> [a]
 -- ^ /O(log n)./ Lookup the value at a key in the map.
 --
 -- The function will return the corrsponding values as a List, or the
 -- empty list if no values are associated witht the given key.
-lookup key (MultiMap (_, _, map)) = maybe [] id (Map.lookup key map)
+lookup key (BagMultiMap (_, _, map)) = maybe [] id (Map.lookup key map)
 
 
-empty :: MultiMap k a
+empty :: BagMultiMap k a
 -- ^ /O(1)./ The empty multimap.
-empty = MultiMap (0, 0, Map.empty)
+empty = BagMultiMap (0, 0, Map.empty)
 
 
-insert :: Ord k => k -> a -> MultiMap k a -> MultiMap k a
+insert :: Ord k => k -> a -> BagMultiMap k a -> BagMultiMap k a
 -- ^ /O(log n)./ Insert a new key and value in the map.
-insert k v (MultiMap (nk, nv, map))
-    | Map.member k map = MultiMap (nk, succ nv, Map.insert k (v : map Map.! k) map)
-    | otherwise = MultiMap (succ nk, succ nv, Map.insert k [v] map)
+insert k v (BagMultiMap (nk, nv, map))
+    | Map.member k map = BagMultiMap (nk, succ nv, Map.insert k (v : map Map.! k) map)
+    | otherwise = BagMultiMap (succ nk, succ nv, Map.insert k [v] map)
 
 
-delete :: (Ord k, Ord e) => k -> e -> MultiMap k a -> MultiMap k a
+delete :: (Ord k, Ord e) => k -> e -> BagMultiMap k a -> BagMultiMap k a
 -- ^ Delete a key/value pair from the map.
 delete = undefined
 
 
-deleteAll :: Ord k => k -> MultiMap k a -> MultiMap k a
+deleteAll :: Ord k => k -> BagMultiMap k a -> BagMultiMap k a
 -- ^ /O(log n)./ Delete a key and all its values from the map.
-deleteAll k m@(MultiMap (nk, nv, map)) = case Map.lookup k map of
-    Just v -> MultiMap (pred nk, nv - fromIntegral (length v), Map.delete k map)
+deleteAll k m@(BagMultiMap (nk, nv, map)) = case Map.lookup k map of
+    Just v -> BagMultiMap (pred nk, nv - fromIntegral (length v), Map.delete k map)
     _      -> m
 
 
-map :: (a -> b) -> MultiMap k a -> MultiMap k b
+map :: (a -> b) -> BagMultiMap k a -> BagMultiMap k b
 -- ^ Map a function over all values in the map.
-map f (MultiMap (nk, nv, map)) = MultiMap (nk, nv, Map.map (P.map f) map)
+map f (BagMultiMap (nk, nv, map)) = BagMultiMap (nk, nv, Map.map (P.map f) map)
 
 
-mapKeys :: Ord k2 => (k1 -> k2) -> MultiMap k1 a -> MultiMap k2 a
+mapKeys :: Ord k2 => (k1 -> k2) -> BagMultiMap k1 a -> BagMultiMap k2 a
 -- ^ mapKeys f s is the multimap obtained by applying f to each key of s.
-mapKeys f (MultiMap (nk, nv, map)) = MultiMap (nk, nv, Map.mapKeys f map)
+mapKeys f (BagMultiMap (nk, nv, map)) = BagMultiMap (nk, nv, Map.mapKeys f map)
 
 
-mapWithKey :: (k -> a -> b) -> MultiMap k a -> MultiMap k b
+mapWithKey :: (k -> a -> b) -> BagMultiMap k a -> BagMultiMap k b
 -- ^ Map a function over all key/value pairs in the map.
-mapWithKey f (MultiMap (nk, nv, map))
-  = MultiMap (nk, nv, Map.mapWithKey (\k -> P.map (f k)) map)
+mapWithKey f (BagMultiMap (nk, nv, map))
+  = BagMultiMap (nk, nv, Map.mapWithKey (\k -> P.map (f k)) map)
 
 
-foldr :: (a -> b -> b) -> b -> MultiMap k a -> b
+foldr :: (a -> b -> b) -> b -> BagMultiMap k a -> b
 -- ^ Fold the values in the map using the given right-associative binary operator.
 foldr f e = P.foldr f e . concat . elems
 
 
-foldl :: (a -> b -> a) -> a -> MultiMap k b -> a
+foldl :: (a -> b -> a) -> a -> BagMultiMap k b -> a
 -- ^  Fold the values in the map using the given left-associative binary operator.
 foldl f e = P.foldl f e . concat . elems
 
 
-foldrWithKey :: (k -> a -> b -> b) -> b -> MultiMap k a -> b
+foldrWithKey :: (k -> a -> b -> b) -> b -> BagMultiMap k a -> b
 -- ^ /O(n)./ Fold the keys and values in the map using the given right-associative
 -- binary operator, taking into account not only the value but also the key.
 foldrWithKey f e = P.foldr (uncurry f) e . toList
 
 
-foldlWithKey :: (a -> k -> b -> a) -> a -> MultiMap k b -> a
+foldlWithKey :: (a -> k -> b -> a) -> a -> BagMultiMap k b -> a
 -- ^ /O(n)./ Fold the keys and values in the map using the given left-associative
 -- binary operator, taking into account not only the value but also the key.
 foldlWithKey f e = P.foldl (\a (k,v) -> f a k v) e . toList
 
 
-elems :: MultiMap k a -> [[a]]
+elems :: BagMultiMap k a -> [[a]]
 -- ^ /O(n)./ Return all elements of the multimap in the
 -- ascending order of their keys.
 --
 -- A list of lists is returned since a key can have
 -- multiple values. Use 'concat' to flatten.
-elems (MultiMap (_, _, map)) = Map.elems map
+elems (BagMultiMap (_, _, map)) = Map.elems map
 
 
-keys :: MultiMap k a -> [k]
+keys :: BagMultiMap k a -> [k]
 -- ^ /O(n)./ Return all keys of the multimap in ascending order.
-keys (MultiMap (_, _, map)) = Map.keys map
+keys (BagMultiMap (_, _, map)) = Map.keys map
 
 
-keysSet :: MultiMap k a -> Set k
+keysSet :: BagMultiMap k a -> Set k
 -- ^ /O(n)./ The set of all keys of the multimap.
-keysSet (MultiMap (_, _, map)) = Map.keysSet map
+keysSet (BagMultiMap (_, _, map)) = Map.keysSet map
 
 
-assocs :: MultiMap k a -> [(k, [a])]
+assocs :: BagMultiMap k a -> [(k, [a])]
 -- ^ /O(n)./ Return all key/value pairs in the multimap
 -- in ascending key order.
-assocs (MultiMap (_, _, map)) = Map.assocs map
+assocs (BagMultiMap (_, _, map)) = Map.assocs map
 
 
-toMap :: MultiMap k a -> Map k [a]
+toMap :: BagMultiMap k a -> Map k [a]
 -- ^ /O(1)./ Return the map of lists.
-toMap (MultiMap (_, _, theUnderlyingMap)) = theUnderlyingMap
+toMap (BagMultiMap (_, _, theUnderlyingMap)) = theUnderlyingMap
 
 
-toMapOfSets :: Ord a => MultiMap k a -> Map k (Set a)
+toMapOfSets :: Ord a => BagMultiMap k a -> Map k (Set a)
 -- ^ /O(k*m*log m) where k is the number of keys and m the
 -- maximum number of elements associated with a single key/
-toMapOfSets (MultiMap (_, _, map)) = Map.map Set.fromList map
+toMapOfSets (BagMultiMap (_, _, map)) = Map.map Set.fromList map
 
 
-toList :: MultiMap k a -> [(k, a)]
+toList :: BagMultiMap k a -> [(k, a)]
 -- ^ Return a flattened list of key/value pairs.
-toList (MultiMap (_, _, map))
+toList (BagMultiMap (_, _, map))
   = concat $ Map.elems $ Map.mapWithKey (\k -> zip (repeat k)) map
 
 
-fromList :: Ord k => [(k, a)] -> MultiMap k a
+fromList :: Ord k => [(k, a)] -> BagMultiMap k a
 -- ^ /O(n*log n)/ Create a multimap from a list of key/value pairs.
 --
 -- > fromList xs == foldr (uncurry insert) empty
 fromList = P.foldr (uncurry insert) empty
 
 
-fromMap :: Map k [a] -> MultiMap k a
+fromMap :: Map k [a] -> BagMultiMap k a
 -- ^ Turns a map of lists into a multimap.
-fromMap map = MultiMap (numKeys, numValues, map)
+fromMap map = BagMultiMap (numKeys, numValues, map)
   where
     numKeys   = fromIntegral $ Map.size map
     numValues = fromIntegral $ Map.foldr (\v s -> length v + s) 0 map
 
 
-findMin :: MultiMap k a -> Maybe k
+findMin :: BagMultiMap k a -> Maybe k
 -- ^ /O(log n)/ Find the minimal key of the multimap.
-findMin (MultiMap (_, _, map))
+findMin (BagMultiMap (_, _, map))
     | Map.null map = Nothing
     | otherwise    = Just $ fst $ Map.findMin map
 
 
-findMax :: MultiMap k a -> Maybe k
+findMax :: BagMultiMap k a -> Maybe k
 -- ^ /O(log n)/ Find the maximal key of the multimap.
-findMax (MultiMap (_, _, map))
+findMax (BagMultiMap (_, _, map))
     | Map.null map = Nothing
     | otherwise    = Just $ fst $ Map.findMax map
 
 
-findMinWithValues :: MultiMap k a -> Maybe (k, [a])
+findMinWithValues :: BagMultiMap k a -> Maybe (k, [a])
 -- ^ /O(log n)/ Find the minimal key and the values associated with it.
-findMinWithValues (MultiMap (_, _, map))
+findMinWithValues (BagMultiMap (_, _, map))
     | Map.null map = Nothing
     | otherwise    = Just $ Map.findMin map
 
 
-findMaxWithValues :: MultiMap k a -> Maybe (k, [a])
+findMaxWithValues :: BagMultiMap k a -> Maybe (k, [a])
 -- ^ /O(log n)/ Find the maximal key and the values associated with it.
-findMaxWithValues (MultiMap (_, _, map))
+findMaxWithValues (BagMultiMap (_, _, map))
     | Map.null map = Nothing
     | otherwise    = Just $ Map.findMax map
 
---
+++ src/Data/SetMultiMap.hs
@@ -0,0 +1,181 @@
+-- Module       : Data.SetMultiMap
+-- Copyright    : (c) Julian Fleischer 2013
+-- License      : MIT (See LICENSE file in cabal package)
+--
+-- Maintainer   : julian.fleischer@fu-berlin.de
+-- Portability  : non-portable (DeriveDataTypeable)
+--
+-- A MultiMap allows the association of multiple values with a single key,
+-- but there are no duplicates per key.
+module Data.SetMultiMap (
+
+    -- * MultiMap type
+    SetMultiMap,
+
+    -- * Querying
+    null,
+    size,
+    keysSize,
+    member,
+    notMember,
+    lookup,
+    (!),
+
+    -- ** Modification
+    insert,
+    delete,
+    deleteAll,
+    alterF,
+
+    -- * Traversal
+    map,
+
+    -- * Construction
+    empty,
+    fromList,
+
+    -- * Conversion
+    elems,
+    keys,
+    toAscList,
+    toMap
+
+  ) where
+
+
+import Prelude hiding (lookup, map, null, foldr, foldl)
+import qualified Prelude as P
+
+import qualified Data.List as List
+
+import qualified Data.Set as Set
+import Data.Set (Set)
+
+import qualified Data.Map.Strict as Map
+import Data.Map.Strict (Map)
+
+import Data.Data
+
+
+-- | A `Set`-based MultiMap with keys @k@ and values @v@.
+newtype SetMultiMap k v 
+  = SetMultiMap { 
+      -- | /O(1)./ Return the underlying map of sets.
+      toMap :: Map k (Set v) 
+    }
+  deriving (Data, Typeable)
+
+instance (Show k, Show v) => Show (SetMultiMap k v) where
+  show m = "fromList " ++ show (toAscList m)
+
+
+null :: SetMultiMap k a -> Bool
+-- ^ /O(1)./ Check whether the multimap is empty or not.
+null = Map.null . toMap
+
+
+size :: SetMultiMap k a -> Int
+-- ^ /O(n)./ The number of elements in the multimap.
+size = length . toAscList
+
+
+keysSize :: SetMultiMap k a -> Int
+-- ^ /O(n)./ The number of keys in the multimap.
+--
+-- As this is a multimap, the number of keys is not
+-- necessarily equal to the number of values.
+keysSize = Map.size . toMap
+
+
+notMember, member :: Ord k => k -> SetMultiMap k a -> Bool
+-- | /O(log n)./ Is the key a member of the multimap?
+member k = Map.member k . toMap
+-- | /O(log n)./ Is the key not a member of the multimap?
+notMember key = not . member key
+
+
+(!) :: Ord k => SetMultiMap k a -> k -> Set a
+-- ^ @flip `lookup`@
+(!) = flip lookup
+
+
+lookup :: Ord k => k -> SetMultiMap k a -> Set a
+-- ^ /O(log n)./ Lookup the value at a key in the map.
+--
+-- The function will return the corresponding values as a Set, or an empty Set if no values are associated with the given key.
+lookup k = maybe Set.empty id . Map.lookup k . toMap
+
+
+empty :: SetMultiMap k a
+-- ^ /O(1)./ The empty multimap.
+empty = SetMultiMap Map.empty
+
+
+insert :: (Ord k, Ord a) => k -> a -> SetMultiMap k a -> SetMultiMap k a
+-- ^ /O(log n)./ Insert a new key and value in the map.
+insert k v = SetMultiMap . Map.insertWith Set.union k (Set.singleton v) . toMap
+
+
+delete :: (Ord k, Ord v) => k -> v -> SetMultiMap k v -> SetMultiMap k v
+-- ^ /O(log n)./ Delete a single key/value pair from the map.
+delete k v = SetMultiMap . Map.update update k . toMap where
+  update set = newSetM where
+    newSetM = if Set.null newSet then Nothing else Just newSet where
+      newSet = Set.delete v set
+
+
+deleteAll :: Ord k => k -> SetMultiMap k a -> SetMultiMap k a
+-- ^ /O(log n)./ Delete a key and all its values from the map.
+deleteAll k = SetMultiMap . Map.delete k . toMap
+
+
+map :: (Ord a, Ord b) => (a -> b) -> SetMultiMap k a -> SetMultiMap k b
+-- ^ /O(n)./ Map a function over all values in the map.
+map f = SetMultiMap . Map.map (Set.map f) . toMap
+
+
+elems :: SetMultiMap k a -> [a]
+-- ^ /O(n)./ Return all elements of the multimap in the
+-- ascending order of their keys.
+elems = List.map snd . toAscList
+
+
+keys :: SetMultiMap k a -> [k]
+-- ^ /O(n)./ Return all keys of the multimap in ascending order.
+keys = List.map fst . toAscList
+
+
+-- | /O(n)./ Convert to an ascending list of key-value pairs.
+toAscList :: SetMultiMap k e -> [(k, e)]
+toAscList = Map.foldrWithKey fu [] . toMap where
+  fu ke elSe li = li' ++ li where
+    li' = Set.foldr (\el li -> (ke, el) : li) [] elSe
+
+
+-- | /O(n)./ Create from a list of key-value pairs.
+fromList :: (Ord e, Ord k) => [(k, e)] -> SetMultiMap k e
+fromList = List.foldr (uncurry insert) empty
+
+
+-- | /O(2 * log n)./ Alter a set of values matched by a key with a functor-function. Allows you to extract and process the matched value, while updating it.
+-- 
+-- This is the most general modification function. All kinds of modification operations can be defined in terms of it.
+-- 
+-- You can consider this function to have the following signature: 
+-- 
+-- > k -> (Set e -> (r, Set e)) -> SetMultiMap k e -> (r, SetMultiMap k e)
+-- 
+-- In fact it is a strictly more general version of it.
+alterF 
+  :: (Ord k, Ord e, Functor f)
+  => k 
+  -> (Set e -> f (Set e)) 
+  -- ^ A function from a set of matching values to a functor on a new set of values. If the returned set is empty the key gets removed from the map. The input values, which aren't present in the output set, get deleted. The output values, which aren't present in the input set, get inserted.
+  -> (SetMultiMap k e -> f (SetMultiMap k e))
+alterF k f m = let
+  items = lookup k m
+  items'F = f items
+  in fmap update items'F where
+    update items' = SetMultiMap $ if Set.null items' 
+      then Map.delete k $ toMap m
+      else Map.insert k items' $ toMap m
